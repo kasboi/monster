c951635e04c0d0c30e991588c4a856ce8dcccedb
Abdullah Solahudeen <solahudeen15@gmail.com>
2021-10-21 09:00:08 +0100

    welp, here we go!

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ preSaleBtn = document.querySelector('.about__button');
 modal = document.querySelector('.modal')
 mnstrBtn = document.querySelector('.mnstr__button')
 mnstrModal = document.querySelector('.mnstr-address__modal')
-mnstrModal_exit = document.querySelector('.mnstr-address__modal--exit')
+mnstrModal_exit = document.querySelector('.mnstr-address__modal--exit-1')
+mnstrModal_exit2 = document.querySelector('.mnstr-address__modal--exit-2')
+
 
 preSaleBtn.addEventListener('click', function(){
     modal.style.display = 'block'
@@ -15,12 +17,15 @@ mnstrBtn.addEventListener('click', function(){
 })
 
 document.addEventListener('keydown', function(event){
-    console.log(event);
     if (event.key === 'Escape') {
         modal.style.display = 'none'
     }
 })
 
 mnstrModal_exit.addEventListener('click', function(){
+    modal.style.display = 'none'
+})
+
+mnstrModal_exit2.addEventListener('click', function(){
     mnstrModal.style.display = 'none'
 })
